95eb3cfb8b536fa5e6671d8fe5b98bcc1b8466e8
Rafael Waldo Delgado Doblas <lord.rafa@gmail.com>
2022-06-04 00:44:46 +0200

    Adding support for J-Link

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOCAL_HOME="$HOME/.onDocker/dev-environments-mcus"
+
 mkdir -p "$HOME/.local/share/icons/hicolor/scalable/apps/"
 mkdir -p "$HOME/.local/share/applications/"
 mkdir -p "$HOME/bin/"
@@ -8,8 +10,8 @@ cp bin/dev-environments-mcus-onDocker.sh "$HOME/bin/"
 curl -L "https://upload.wikimedia.org/wikipedia/commons/0/07/IC_SMD16SQ_filled.svg" -o "$HOME/.local/share/icons/hicolor/scalable/apps/dev-environments-mcus-onDocker.svg"
 cp bin/dev-environments-mcus-onDocker.desktop "$HOME/.local/share/applications/"
 
-"$HOME/bin/dev-environments-mcus-onDocker.sh" echo "Visual Studio Code MCU Development Environment on Docker installed."
+"$HOME/bin/dev-environments-mcus-onDocker.sh" echo "MCU Development Environment on Docker installed."
 
 echo "Make sure that $HOME/bin is in your PATH at .bashrc or similar."
 echo "Application icon may require to logout to show up properly."
-
+echo "In order to add Segger's J-Link support you will need to manually copy J-Link binaries to $LOCAL_HOME/JLINK folder"
