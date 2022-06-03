6621bcd20b8c9cd62fae1205b5fc1d4c9cdaea57
Rafael Waldo Delgado Doblas <lord.rafa@gmail.com>
2022-06-04 01:00:34 +0200

    Adding usb support

--- bin/dev-environments-mcus-onDocker.sh
+++ bin/dev-environments-mcus-onDocker.sh
@@ -8,19 +8,19 @@ CONTAINER_NAME="dev-environments-mcus"
 
 OPTS=" "
 
-# Uncomment following line for GUI support
-# OPTS="${OPTS} --env=DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix"
-
-# Uncomment following lines for USB support (replace XXX by USB device mayor number) 
-# OPTS="${OPTS} --device /dev/kvm --group-add kvm"
-# OPTS="${OPTS} -v /dev/bus/usb:/dev/bus/usb:ro --group-add plugdev --device-cgroup-rule 'c XXX:* rwm'"
-
-
 if [ -z "$@" ]; then
-  CONTAINER_ID=$(docker run --rm -d --name="$CONTAINER_NAME" ${OPTS} -v "$LOCAL_HOME:/App_Home" -v "$LOCAL_WORKSPACE:/App_Home/mcu-projects" lordrafa/dev-environments-mcus tail -f /dev/null)
+
+  docker run --rm -d --name="$CONTAINER_NAME" \
+  -v /dev/bus/usb:/dev/bus/usb:ro --group-add plugdev --device-cgroup-rule 'c 189:* rwm' \
+  -v "$LOCAL_HOME:/App_Home" -v "$LOCAL_WORKSPACE:/App_Home/mcu-projects" \
+  lordrafa/dev-environments-mcus tail -f /dev/null
+
   hex=$(printf \{\"containerName\"\:\""$CONTAINER_NAME"\"\} | od -A n -t x1 | tr -d '[\n\t ]')
   code --folder-uri vscode-remote://attached-container+${hex}/App_Home/mcu-projects
+
 else
+
   docker run --rm -i ${OPTS} -v "$LOCAL_HOME:/App_Home" -v "$LOCAL_WORKSPACE:/App_Home/mcu-projects" lordrafa/dev-environments-mcus "$@"
+
 fi
 
--- install.sh
+++ install.sh
@@ -9,9 +9,13 @@ mkdir -p "$HOME/bin/"
 cp bin/dev-environments-mcus-onDocker.sh "$HOME/bin/"
 curl -L "https://upload.wikimedia.org/wikipedia/commons/0/07/IC_SMD16SQ_filled.svg" -o "$HOME/.local/share/icons/hicolor/scalable/apps/dev-environments-mcus-onDocker.svg"
 cp bin/dev-environments-mcus-onDocker.desktop "$HOME/.local/share/applications/"
+echo "Root access must be granted in order to install udev rules to allow usb access"
+sudo cp bin/99-platformio-udev.rules /etc/udev/rules.d/
 
 "$HOME/bin/dev-environments-mcus-onDocker.sh" echo "MCU Development Environment on Docker installed."
 
 echo "Make sure that $HOME/bin is in your PATH at .bashrc or similar."
 echo "Application icon may require to logout to show up properly."
 echo "In order to add Segger's J-Link support you will need to manually copy J-Link binaries to $LOCAL_HOME/JLINK folder"
+echo "Remember that you must have VSCode already installed and it must be on the PATH"
+
