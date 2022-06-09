3128264d0cd47923916c2d08bf137b0b1c60f5ca
Rafael Waldo Delgado Doblas <lord.rafa@gmail.com>
2022-06-09 23:13:47 +0200

    More serial support fixes

--- install.sh
+++ install.sh
@@ -9,8 +9,10 @@ mkdir -p "$HOME/bin/"
 cp bin/dev-environments-mcus-onDocker.sh "$HOME/bin/"
 curl -L "https://upload.wikimedia.org/wikipedia/commons/0/07/IC_SMD16SQ_filled.svg" -o "$HOME/.local/share/icons/hicolor/scalable/apps/dev-environments-mcus-onDocker.svg"
 cp bin/dev-environments-mcus-onDocker.desktop "$HOME/.local/share/applications/"
-echo "Root access must be granted in order to install udev rules to allow usb access"
+echo "Root access must be granted in order to install udev rules and scripts to allow usb tty access"
 sudo cp bin/99-platformio-udev.rules /etc/udev/rules.d/
+sudo cp bin/99-dev-environments-mcus-tty.rules /etc/udev/rules.d/
+sudo cp bin/dev-environments-mcus-tty.sh /usr/local/bin/
 
 "$HOME/bin/dev-environments-mcus-onDocker.sh" echo "MCU Development Environment on Docker installed."
 
--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,9 @@ rm -rf "$HOME/bin/dev-environments-mcus-onDocker.sh"
 rm -rf "$HOME/.local/share/icons/hicolor/scalable/apps/dev-environments-mcus-onDocker.svg"
 rm -rf "$HOME/.local/share/applications/dev-environments-mcus-onDocker.desktop"
 rm -rf "$HOME/.onDocker/dev-environments-mcus"
+sudo rm -rf "/etc/udev/rules.d/99-platformio-udev.rules"
+sudo rm -rf "/etc/udev/rules.d/99-dev-environments-mcus-tty.rules"
+sudo rm -rf "/usr/local/bin/dev-environments-mcus-tty.sh"
 
 docker image rm lordrafa/dev-environments-mcus
 
