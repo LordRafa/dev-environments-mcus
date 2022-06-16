fb897167db1c434136acded42505311f19ffc2ca
Rafael Waldo Delgado Doblas <lord.rafa@gmail.com>
2022-06-17 00:04:19 +0200

    More usb support fixes

--- bin/dev-environments-mcus-udev.sh
+++ bin/dev-environments-mcus-udev.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-echo "Usb event: $ACTION $DEVNAME $MAJOR $MINOR" >> /tmp/docker_tty.log
-
 if [ ! -z "$(docker ps -qf name=dev-environments-mcus)" ]; then
 
   if [ "$ACTION" == "add" ]; then
 
+    docker exec -u 0 dev-environments-mcus mkdir -p $(dirname $DEVNAME)
     docker exec -u 0 dev-environments-mcus mknod $DEVNAME c $MAJOR $MINOR
     docker exec -u 0 dev-environments-mcus chmod -R 777 $DEVNAME
-    echo "Adding $DEVNAME to docker" >> /tmp/docker_tty.log
 
   elif [ "$ACTION" == "remove" ]; then
 
     docker exec -u 0 dev-environments-mcus rm $DEVNAME
-    echo "Removing $DEVNAME from docker" >> /tmp/docker_tty.log
+    docker exec -u 0 dev-environments-mcus rmdir -p $(dirname $DEVNAME)
 
   fi
 
 fi
+
